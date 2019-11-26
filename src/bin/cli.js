08f522ab74258db256356f1c07c9bd43aca005bd
tavira <quramolt@gmail.com>
2019-11-26 20:00:57 +0300

    simplify function name

--- src/bin/cli.js
+++ src/bin/cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import program from 'commander';
 
-import downloadPageWithResources from '..';
+import downloadPage from '..';
 
 const description = 'This program downloads page from the specified URL';
 
@@ -9,7 +9,7 @@ program
   .description(description)
   .option('--output <value>', 'destination folder')
   .arguments('<url>')
-  .action(url => downloadPageWithResources(url, program.output)
+  .action(url => downloadPage(url, program.output)
     .then(() => console.log('\nPage downloaded'))
     .catch((e) => {
       console.error(e.message);
